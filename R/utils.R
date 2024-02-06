b5e6620af5b4865b712b671c312ba97b32624035
hlageek <radim.hladik@fulbrightmail.org>
2024-02-06 13:46:57 +0000

    rework file names

--- R/utils.R
+++
@@ -1,7 +0,0 @@
-run_subpipeline <- function(script, store, depend_on){
-        targets::tar_make(
-        script = script,
-        store = store
-        )
-        c("Last compiled" = Sys.time())
-}
